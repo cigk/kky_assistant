62d3fc6a4e96994f2ba6e8491199b7c6e1f5ede7
jinlu <jinlu@kuaikuaiyu.com>
2016-05-23 14:31:37 +0800

    1. kkylib.c opt

--- app/src/main/jni/kkylib.c
+++ app/src/main/jni/kkylib.c
@@ -6,19 +6,6 @@
 
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG , "KKY", __VA_ARGS__)
 
-jstring CharTojstring(JNIEnv *env, char *str) {
-    jsize len = strlen(str);
-
-    jclass clsstring = (*env)->FindClass(env, "java/lang/String");
-    jstring strencode = (*env)->NewStringUTF(env, "GB2312");
-
-    jmethodID mid = (*env)->GetMethodID(env, clsstring, "<init>", "([BLjava/lang/String;)V");
-    jbyteArray barr = (*env)->NewByteArray(env, len);
-
-    (*env)->SetByteArrayRegion(env, barr, 0, len, (jbyte *) str);
-    return (jstring)(*env)->NewObject(env, clsstring, mid, barr, strencode);
-}
-
 char *Jstring2CStr(JNIEnv *env, jstring jstr) {
     char *rtn = NULL;
     jclass clsstring = (*env)->FindClass(env, "java/lang/String");
@@ -39,7 +26,8 @@ char *Jstring2CStr(JNIEnv *env, jstring jstr) {
 
 JNIEXPORT jstring JNICALL Java_com_kuaikuaiyu_assistant_utils_JniUtil_sign
         (JNIEnv *env, jobject obj, jstring str) {
-    char *cstr = Jstring2CStr(env, str);
+//    char *cstr = Jstring2CStr(env, str);
+    char* cstr = (*env)->GetStringUTFChars(env, str, 0);
     char *signature[64];
     signs(cstr, signature);
     return (*env)->NewStringUTF(env, signature);
